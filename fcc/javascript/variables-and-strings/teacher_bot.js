d1997d94ab9866ffd235fa5111567a9c60595f71
fennerii <jonathanfennerii@gmail.com>
2026-03-29 19:51:49 -0400

    started teacher bot

---
+++ fcc/javascript/variables-and-strings/teacher_bot.js
@@ -0,0 +1,13 @@
+console.log("Hi there!");
+
+const botName = "teacherBot";
+
+const greeting = `My name is ${botName}.`;
+console.log(greeting);
+
+const subject = "JavaScript";
+const topic = "strings";
+
+const sentence = `Today, you will learn about ${topic} in ${subject}.`;
+console.log(sentence);
+
